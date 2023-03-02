4e376b6bfd436cff87f31fa1a2b99c3c7e913575
Racheltembi <114146038+Racheltembi@users.noreply.github.com>
2023-03-02 18:52:35 +0000

    update

--- app.java
+++ app.java
@@ -1,3 +1,5 @@
 This script is to deploy a java application
 follow good coding standard
 testing must be done to ensure quality delivery
+ensure to always pull back to working area so u get updates from all team mates
+if ok remeber to aprove at all given times
