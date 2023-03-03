0ff849c58329ccd2f1e7953059bc5395b4082864
Rachel <rachelndah2022@gmail.com>
2023-03-03 11:35:54 +0000

    Jehova overdo ehhh

--- app.java
+++ app.java
@@ -3,3 +3,4 @@ follow good coding standard
 testing must be done to ensure quality delivery
 ensure to always pull back to working area so u get updates from all team mates
 if ok remeber to aprove at all given times
+you can create the feature branch from master.
