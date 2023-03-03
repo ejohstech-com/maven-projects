d04d48db89d46906409331b93d99218766f58df5
Racheltembi <114146038+Racheltembi@users.noreply.github.com>
2023-03-03 12:11:58 +0100

    updated march

--- bugs.py
+++ bugs.py
@@ -5,3 +5,8 @@ i must be serious
 who no know go know
 God is great
 and worthy to be praise
+Lord we thank you for healing David
+we bless you for exempting Daniela
+we bless u for healing Davina
+receive all the glory honour and adoration
+amen and amen
